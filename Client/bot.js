d5d65d22bc95292e3735623cca28857da7f3ba7c
carlosbasan2011@hotmail.com <24129912c>
2021-06-08 11:33:15 -0500

    Bot yottecats

--- Client/bot.js
+++ Client/bot.js
@@ -1,11 +1,4 @@
 const Discord = require("discord.js");
-const aws = require('aws-sdk');
-
-let s3 = new aws.S3({
-    secretAccessKey: process.env.TOKEN,
-
-}, console.log("dsadad"+process.env.TOKEN));
-
 const client = new Discord.Client();
 const config = require('../config.json');
 const prefix = config.PREFIX;
@@ -15,8 +8,7 @@ const ready = require('./ready/ready')
 
 
 function start () {
-    console.log(s3.config.secretAccessKey)
-    client.login(s3.config.accessKeyId).then(() => {console.log("Funcionando... ")}).catch(err =>console.log(err))
+    client.login(config.TOKEN).then(() => {console.log("Funcionando... ")}).catch(err =>console.log(err))
     messageUpdate.messageUpdate(client)
     mainMessage.mainMessage(client, prefix, Discord)
     ready.inicia(client, Discord)
